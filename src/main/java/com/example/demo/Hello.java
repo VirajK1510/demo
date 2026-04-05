34353d396ce265bc9fa7d3abb567875eacf4b479
Viraj Khandelwal <khandelwalviraj1510@gmail.com>
2026-04-05 23:08:16 +0530

    Get API

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -1,14 +1,28 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Hello {
 
     @GetMapping("/api")
-    String abc() {
-        return "Hello Java Spring boot is working";
+    List<DTO_mode> abc() {
+        List<DTO_mode> modes = new ArrayList<>();
+
+        DTO_mode dt = new DTO_mode();
+
+        dt.setId(20);
+        dt.setIsActive("True");
+        dt.setStart_time("12-02-2026");
+        dt.setEnd_time("13-12-2026");
+
+        modes.add(dt);
+
+        return modes;
     }
 }
